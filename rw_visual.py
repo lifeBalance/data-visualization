e3f6cdf4cb2a07baca6afdef18aa38f765735bbd
javi <3051496+lifeBalance@users.noreply.github.com>
2023-07-15 21:45:51 +0300

    emphasize beginning and end of the walk

--- rw_visual.py
+++ rw_visual.py
@@ -10,11 +10,23 @@ while True:
     rw.fill_walk()
 
     # Plot the points in the walk.
-    plt.style.use('classic')
+    plt.style.use("classic")
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=14)
-    ax.set_aspect('equal')
+    # Generate a list of numbers with the same amounts of points in the walk.
+    point_numbers = range(rw.num_points)
+    ax.scatter(
+        rw.x_values,
+        rw.y_values,
+        c=point_numbers,
+        cmap=plt.cm.Blues,
+        edgecolors="none",
+        s=14,
+    )
+    ax.set_aspect("equal")
+    # Emphasize first and last points of the walk
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=50)
     plt.show()
-    keep_running = input('Make another walk? (y/n): ')
-    if keep_running == 'n':
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == "n":
         break
