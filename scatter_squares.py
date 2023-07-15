e51bc1e0594884f3dda6b81de65944a48b1ad57f
javi <3051496+lifeBalance@users.noreply.github.com>
2023-07-15 18:40:29 +0300

    plot series of points

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt # alias the pyplot module to plt (convention)
 
-input_values = [1, 2, 3, 4, 5]  # values used for x axis
+x_values = [1, 2, 3, 4, 5]  # values used for x axis
 # 1st 5 terms of a square number sequence.
-square_seq = [1, 4, 9, 16, 25]  # values used for y axis
+y_values = [1, 4, 9, 16, 25]  # values used for y axis
 
 # Set the theme
 plt.style.use('seaborn')
@@ -13,7 +13,7 @@ plt.style.use('seaborn')
 fig, ax = plt.subplots()
 
 # Plot the point at coordinates 2, 4.
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, s=100)
 
 # Set chart title and label axes.
 ax.set_title('Square Numbers', fontsize=24)
