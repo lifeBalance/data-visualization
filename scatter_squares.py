151b7c12cebec9fe38188eb6fe80f5de7bcf9160
javi <3051496+lifeBalance@users.noreply.github.com>
2023-07-15 18:50:54 +0300

    playing with styles

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt # alias the pyplot module to plt (convention)
 
-x_values = [1, 2, 3, 4, 5]  # values used for x axis
-# 1st 5 terms of a square number sequence.
-y_values = [1, 4, 9, 16, 25]  # values used for y axis
+x_values = range(1, 1001)  # generate values from 1 to 1001 (1001 not included)
+y_values = [x**2 for x in x_values]  # list comprehension
 
 # Set the theme
 plt.style.use('seaborn')
@@ -13,12 +12,23 @@ plt.style.use('seaborn')
 fig, ax = plt.subplots()
 
 # Plot the point at coordinates 2, 4.
-ax.scatter(x_values, y_values, s=100)
+# ax.scatter(x_values, y_values, color='red', s=10)       # red
+# ax.scatter(x_values, y_values, color=(0, 0.8, 0), s=10) # green
+# colormap with light blue for low values and dark blue for high values
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axes.
 ax.set_title('Square Numbers', fontsize=24)
 ax.set_xlabel('Value', fontsize=14)
 ax.set_ylabel('Square of Value', fontsize=14)
+
+# Set the range for each axis. It takes an array of 4 values:
+# - minimum value for x-axis
+# - maximum value for x-axis
+# - minimum value for y-axis
+# - maximum value for y-axis
+ax.axis([0, 1100, 0, 1_100_000])
+ax.ticklabel_format(style='plain') # No scientific notation.
 
 # Set size of tick labels.
 ax.tick_params(labelsize=14)
